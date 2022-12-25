91bfcea32069a6be07dc4ba5a02d8772cf0dbe5e
sherif magdy <sh1k0002011@gmail.com>
2022-12-25 14:55:36 +0200

    Fix bug when toggleClass property is a string

--- src/plugins/ToggleClass.js
+++ src/plugins/ToggleClass.js
@@ -2,59 +2,59 @@ import { defaultToggleClassParams } from '../constants';
 import { is, mergeOptions, splitStr } from '../helpers';
 
 class ToggleClass {
-  constructor(it) {
-    this._registerIntersectionTrigger(it);
-    return this;
-  }
+	constructor(it) {
+		this._registerIntersectionTrigger(it);
+		return this;
+	}
 
-  _registerIntersectionTrigger(intersectionTrigger) {
-    this._it = intersectionTrigger;
-    this._utils = this._it._utils;
-  }
+	_registerIntersectionTrigger(intersectionTrigger) {
+		this._it = intersectionTrigger;
+		this._utils = this._it._utils;
+	}
 
-  toggle(trigger, toggleClass, eventIndex) {
-    for (const { targets, toggleActions, classNames } of toggleClass) {
-      if ('none' === toggleActions[eventIndex]) continue;
-      classNames.forEach((className) => {
-        if (targets) {
-          targets.forEach((target) => target.classList[toggleActions[eventIndex]](className));
-          return;
-        }
-        trigger.classList[toggleActions[eventIndex]](className);
-      });
-    }
-  }
+	toggle(trigger, toggleClass, eventIndex) {
+		for (const { targets, toggleActions, classNames } of toggleClass) {
+			if ('none' === toggleActions[eventIndex]) continue;
+			classNames.forEach((className) => {
+				if (targets) {
+					targets.forEach((target) => target.classList[toggleActions[eventIndex]](className));
+					return;
+				}
+				trigger.classList[toggleActions[eventIndex]](className);
+			});
+		}
+	}
 
-  parse(params) {
-    let toggleClass = [];
+	parse(params) {
+		let toggleClass = [];
 
-    if (is.string(params)) {
-      const mergedParams = mergeOptions(defaultToggleClassParams, {
-        classNames: splitStr(params),
-      });
-      toggleClass.push([mergedParams]);
-      return toggleClass;
-    }
+		if (is.string(params)) {
+			const mergedParams = mergeOptions(defaultToggleClassParams, {
+				classNames: splitStr(params),
+			});
+			toggleClass.push(mergedParams);
+			return toggleClass;
+		}
 
-    if (is.array(params)) {
-      toggleClass = params.map((obj) => {
-        const mergedParams = mergeOptions(defaultToggleClassParams, obj);
-        const { targets, classNames, toggleActions } = mergedParams;
+		if (is.array(params)) {
+			toggleClass = params.map((obj) => {
+				const mergedParams = mergeOptions(defaultToggleClassParams, obj);
+				const { targets, classNames, toggleActions } = mergedParams;
 
-        targets && (mergedParams.targets = this._utils.parseQuery(targets, 'targets'));
-        classNames && (mergedParams.classNames = splitStr(classNames));
-        is.string(toggleActions) && (mergedParams.toggleActions = splitStr(toggleActions));
-        return mergedParams;
-      });
-    }
+				targets && (mergedParams.targets = this._utils.parseQuery(targets, 'targets'));
+				classNames && (mergedParams.classNames = splitStr(classNames));
+				is.string(toggleActions) && (mergedParams.toggleActions = splitStr(toggleActions));
+				return mergedParams;
+			});
+		}
 
-    return toggleClass;
-  }
+		return toggleClass;
+	}
 
-  kill = () => {
-    this._it = null;
-    this._utils = null;
-  };
+	kill = () => {
+		this._it = null;
+		this._utils = null;
+	};
 }
 ToggleClass.pluginName = 'toggleClass';
 
