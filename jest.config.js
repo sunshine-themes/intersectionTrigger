7637b9ecee04b864ac546b35d4dd9a099e9c47ab
sherif magdy <sh1k0002011@gmail.com>
2023-03-10 12:07:25 +0200

    Test: refactor 'jest.config'

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
 module.exports = {
 	preset: 'ts-jest',
+	roots: ['<rootDir>/src/', '<rootDir>/tests/'],
 	testEnvironment: 'node',
 	testMatch: ['**/*.test.(js|jsx|ts|tsx)'],
 	moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-	collectCoverage: true,
 	coverageDirectory: 'coverage',
-	collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
-	coverageReporters: ['text', 'lcov']
+	collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts']
 };
