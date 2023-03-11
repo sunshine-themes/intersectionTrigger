9b2c6ca1a60dd4fa7c375de1196dc98f2bd8043b
sherif magdy <sh1k0002011@gmail.com>
2023-03-11 11:14:07 +0200

    Test: remove 'roots' property form 'jest.config'

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	preset: 'ts-jest',
-	roots: ['<rootDir>/tests/'],
 	testEnvironment: 'node',
 	testMatch: ['**/*.test.(js|jsx|ts|tsx)'],
 	moduleFileExtensions: ['ts', 'js', 'json', 'node']
