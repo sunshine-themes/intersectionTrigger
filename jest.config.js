15a8e36ed0cb26581afc03bb1182fbf274997c20
sherif magdy <sh1k0002011@gmail.com>
2023-03-11 10:27:53 +0200

    CI: test coverage reports

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 module.exports = {
 	preset: 'ts-jest',
-	roots: ['<rootDir>/src/', '<rootDir>/tests/'],
+	roots: ['<rootDir>/tests/'],
 	testEnvironment: 'node',
 	testMatch: ['**/*.test.(js|jsx|ts|tsx)'],
-	moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-	coverageDirectory: 'coverage',
-	collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts']
+	moduleFileExtensions: ['ts', 'js', 'json', 'node']
 };
