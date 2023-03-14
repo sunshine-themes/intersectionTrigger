0f2a2316a7678c557976f3a4cebaf6b541d809f4
sherif magdy <sh1k0002011@gmail.com>
2023-03-14 14:29:19 +0200

    Test: refactor 'jest.config' and add 'jsdpm' as 'testEnvironment'

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
 	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	setupFilesAfterEnv: ['./tests/_template.ts'],
 	roots: ['<rootDir>/src/', '<rootDir>/tests/'],
-	collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/types/**']
+	collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/types/**'],
+	verbose: true,
+	testEnvironmentOptions: {
+		pretendToBeVisual: true
+	}
 };
