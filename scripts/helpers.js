32b40f608b566304303689041b031026d1ea070e
sherif magdy <sh1k0002011@gmail.com>
2023-03-14 09:34:16 +0200

    Style(scripts): reformat 'helpers.js'

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,16 +1,16 @@
-const capitalize = (name) => {
-  return name
-    .split('-')
-    .map((word) => {
-      return word
-        .split('')
-        .map((char, index) => {
-          if (index === 0) return char.toUpperCase();
-          return char;
-        })
-        .join('');
-    })
-    .join('');
+const capitalize = name => {
+	return name
+		.split('-')
+		.map(word => {
+			return word
+				.split('')
+				.map((char, index) => {
+					if (index === 0) return char.toUpperCase();
+					return char;
+				})
+				.join('');
+		})
+		.join('');
 };
 
 module.exports = { capitalize };
