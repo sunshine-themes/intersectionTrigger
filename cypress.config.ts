9726272f5ee885dc49cbcd5f35a13e6335c79436
sherif magdy <sh1k0002011@gmail.com>
2023-03-20 22:34:01 +0200

    Test: switch to cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
 	projectId: 'qwazj6',
 	e2e: {
-		baseUrl: 'http://localhost:3000',
-		setupNodeEvents(on, config) {
-			// implement node event listeners here
-		}
+		baseUrl: 'http://localhost:3000/cypress/e2e/templates'
 	}
 });
