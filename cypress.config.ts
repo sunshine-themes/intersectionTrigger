175bd8c328bdf420d017a62aa0d6ec1315c6fb78
sherif magdy <sh1k0002011@gmail.com>
2023-04-01 04:02:46 +0200

    Test: change 'baseUrl' property in 'cypress.config'

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
 	projectId: 'qwazj6',
 	e2e: {
-		baseUrl: 'http://localhost:3000/cypress/e2e/templates'
+		baseUrl: 'http://localhost:3000/cypress/html'
 	}
 });
