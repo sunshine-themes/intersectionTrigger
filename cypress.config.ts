b69e8c437a7ad63cb66c1a389c6344ff658ebb7d
sherif magdy <sh1k0002011@gmail.com>
2023-04-04 07:10:32 +0200

    Test: added 'codeCoverage' cypress plugin

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'cypress';
+import registerCodeCoverageTasks from '@cypress/code-coverage/task';
 
 export default defineConfig({
 	projectId: 'qwazj6',
 	e2e: {
-		baseUrl: 'http://localhost:3000/cypress/html'
+		baseUrl: 'http://localhost:3000/cypress/html',
+		setupNodeEvents(on, config) {
+			registerCodeCoverageTasks(on, config);
+
+			// It's IMPORTANT to return the config object
+			// with any changed environment variables
+			return config;
+		}
 	}
 });
